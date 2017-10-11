ebc746fa72012e416f01e811ebbdd96f59df2c7e
ILIJAXPS\ivukotic <ivukotic@cern.ch>
2017-10-10 22:07:19 -0500

    changing AD code for continous running.

--- AD_nn.py
+++ AD_nn.py
@@ -34,99 +34,111 @@ sub = sub * Hour()
 
 es = Elasticsearch([{'host':'atlas-kibana.mwt2.org', 'port':9200}],timeout=60)
 
-my_query = {
-    "size": 1,
-    "query":{
-       "bool":{
-            "must":[
-                {"term" : { "processed" : "no" }},
-                {"term" : { "_type" : alarm_type }}
-            ]
-        }
-    },
-    "sort" : [
-      {"timestamp" : {"order" : "asc"}}
-    ]
-}
 
-
-res = es.search(index=alarm_index, body=my_query, request_timeout=120)
-#print(res)
-job_id = res['hits']['hits'][0]['_id']
-job_source = res['hits']['hits'][0]['_source']
-job_timestamp = job_source['timestamp']
-site = job_source['site']
-endpoint = job_source['endpoint']
-print ('Processing id:',job_id, '\ttimebin:', job_timestamp, '\tsite:', site, '\tendpoint:', endpoint)
-
-# setting a lock on the job
-es.update(index=alarm_index, doc_type=alarm_type, id=job_id, body={"doc": {"processed": "pending"}})
-
-# getting actual perfsonar data
-end = pd.Timestamp(job_timestamp)
-start = end - ref - sub
-print('start:', start, '\tend:', end)
-
-indices = "network_weather-2017.*"
-
-my_query = {
-    'query': { 
-       'bool':{
-            'must':[
-                    {'range': {'timestamp': {'gte': start.strftime('%Y%m%dT%H%M00Z'), 'lt': end.strftime('%Y%m%dT%H%M00Z')}}},
-                    {'bool':
-                        {'should':[
-                            {'term': {'src': endpoint}},
-                            #{'term': {'src': srcSiteThroughputServer}},
-                            #{'term': {'src': destSiteOWDServer}},
-                            #{'term': {'src': destSiteThroughputServer}}
-                        ]}
-                    },
-                    {'bool':
-                        {'should':[
-                            {'term': {'_type': 'packet_loss_rate'}},
-                            #{'term': {'_type': 'latency'}},
-                            #{'term': {'_type': ''}},
-                            #{'term': {'_type': ''}}
-                        ]}
-                    }
-                
-                   ]
+def getWorkload():
+    my_query = {
+        "size": 1,
+        "query":{
+        "bool":{
+                "must":[
+                    {"term" : { "processed" : "no" }},
+                    {"term" : { "_type" : alarm_type }}
+                ]
             }
-        }
+        },
+        "sort" : [
+        {"timestamp" : {"order" : "asc"}}
+        ]
     }
 
 
-scroll = scan(client=es, index=indices, query=my_query)
+    res = es.search(index=alarm_index, body=my_query, request_timeout=120)
 
+    #print(res)
+    hits = res['hits']['hits']
+    if len(hits)==0:
+        print('All done.')
+        return (0,0,0)
 
-#scan the data
-count = 0
-allData={} # will be like this: {'dest_host':[[timestamp],[value]], ...} 
-for res in scroll:
-#     if count<2: print(res) 
-    if not count%100000: print(count)
-    if count>1000000: break
-    dst = res['_source']['dest'] # old data - dest, new data - dest_host
-    if dst not in allData: allData[dst]=[[],[]]
-    allData[dst][0].append(res['_source']['timestamp'] )
-    allData[dst][1].append(res['_source']['packet_loss'])
+    job_id = res['hits']['hits'][0]['_id']
+    job_source = res['hits']['hits'][0]['_source']
+    job_timestamp = job_source['timestamp']
+    site = job_source['site']
+    endpoint = job_source['endpoint']
+    print ('Processing id:',job_id, '\ttimebin:', job_timestamp, '\tsite:', site, '\tendpoint:', endpoint)
+
+    # setting a lock on the job
+    es.update(index=alarm_index, doc_type=alarm_type, id=job_id, body={"doc": {"processed": "pending"}})
+
     
-    count=count+1
+    # getting actual perfsonar data
+    end = pd.Timestamp(job_timestamp)
+    start = end - ref - sub
+    print('start:', start, '\tend:', end)
 
-dfs=[]
-for dest,data in allData.items():
-    ts=pd.to_datetime(data[0],unit='ms')
-    df=pd.DataFrame({dest:data[1]}, index=ts )
-    df.sort_index(inplace=True)
-    df.index = df.index.map(lambda t: t.replace(second=0))
-    df = df[~df.index.duplicated(keep='last')]
-    dfs.append(df)
-    #print(df.head(2))
-print('docs read:', count)
+    indices = "network_weather-2017.*"
 
-full_df = pd.concat(dfs, axis=1)
-print(full_df.shape)
+    my_query = {
+        'query': { 
+        'bool':{
+                'must':[
+                        {'range': {'timestamp': {'gte': start.strftime('%Y%m%dT%H%M00Z'), 'lt': end.strftime('%Y%m%dT%H%M00Z')}}},
+                        {'bool':
+                            {'should':[
+                                {'term': {'src': endpoint}},
+                                #{'term': {'src': srcSiteThroughputServer}},
+                                #{'term': {'src': destSiteOWDServer}},
+                                #{'term': {'src': destSiteThroughputServer}}
+                            ]}
+                        },
+                        {'bool':
+                            {'should':[
+                                {'term': {'_type': 'packet_loss_rate'}},
+                                #{'term': {'_type': 'latency'}},
+                                #{'term': {'_type': ''}},
+                                #{'term': {'_type': ''}}
+                            ]}
+                        }
+                    
+                    ]
+                }
+            }
+        }
+
+
+    scroll = scan(client=es, index=indices, query=my_query)
+
+
+    #scan the data
+    count = 0
+    allData={} # will be like this: {'dest_host':[[timestamp],[value]], ...} 
+    for res in scroll:
+    #     if count<2: print(res) 
+        if not count%100000: print(count)
+        if count>1000000: break
+        dst = res['_source']['dest'] # old data - dest, new data - dest_host
+        if dst not in allData: allData[dst]=[[],[]]
+        allData[dst][0].append(res['_source']['timestamp'] )
+        allData[dst][1].append(res['_source']['packet_loss'])
+        
+        count=count+1
+
+    dfs=[]
+    for dest,data in allData.items():
+        ts=pd.to_datetime(data[0],unit='ms')
+        df=pd.DataFrame({dest:data[1]}, index=ts )
+        df.sort_index(inplace=True)
+        df.index = df.index.map(lambda t: t.replace(second=0))
+        df = df[~df.index.duplicated(keep='last')]
+        dfs.append(df)
+        #print(df.head(2))
+    print('docs read:', count)
+
+    full_df = pd.concat(dfs, axis=1)
+    print(full_df.shape)
+    # fix NANs
+    full_df.fillna(0, inplace=True)
+    return (job_id, end, full_df)
 
 def scaled_accuracy(accuracy, ref_samples, sub_samples):
     chance = float(ref_samples)/(ref_samples+sub_samples)
@@ -137,43 +149,44 @@ def scaled_accuracy(accuracy, ref_samples, sub_samples):
 
 # create Network Model
 class ANN(object):
-    def __init__(self, n_series):
-        self.n_series = n_series
-        self.df = None
-        self.auc_df = None
-        
+    def __init__(self, end, data):
+        self.n_series = data.shape[1]
+        self.df = data
+        self.lend = end
         self.nn = Sequential()
         self.nn.add(Dense(units=n_series*2, input_shape=(n_series,), activation='relu' ))
-#       self.nn.add(Dropout(0.5))
+    #   self.nn.add(Dropout(0.5))
         self.nn.add(Dense(units=n_series, activation='relu'))
-#       self.nn.add(Dropout(0.5))
+    #   self.nn.add(Dropout(0.5))
         self.nn.add(Dense(units=1, activation='sigmoid'))
-#       self.nn.compile(loss='hinge', optimizer='sgd', metrics=['binary_accuracy'])
-#       self.nn.compile(loss='mse',optimizer='rmsprop', metrics=['accuracy'])
+    #   self.nn.compile(loss='hinge', optimizer='sgd', metrics=['binary_accuracy'])
+    #   self.nn.compile(loss='mse',optimizer='rmsprop', metrics=['accuracy'])
         self.nn.compile(loss='binary_crossentropy',optimizer='rmsprop', metrics=['accuracy'])
-#       self.nn.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['binary_accuracy'])
+    #   self.nn.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['binary_accuracy'])
         self.nn.summary()
-    
-    def set_data(self, df):
-        self.df = df
+        
  
     def check_for_anomaly(self):
     
         lstart = self.df.index.min()
-        lend = self.df.index.max()
+        # lend = self.df.index.max()
 
         #round start 
         lstart.seconds=0
         lstart.minutes=0
-        print(lstart, lend)
+        print(lstart, self.lend)
         
-        ti = lstart + ref
+        ti = self.lend - sub 
+        # ti = lstart + ref
         
         startt = time()
         
         ref_df = self.df[ (self.df.index < ti)]
         sub_df = self.df[(self.df.index >= ti)]
     
+        if ref_df.shape[0] < 32 or sub_df.shape[0]<32:
+            return -999
+
         y_ref = pd.Series([0] * ref_df.shape[0])
         X_ref = ref_df
     
@@ -204,14 +217,20 @@ class ANN(object):
         print("took:", time()-startt)
          
         return scaled_accuracy(loss_and_metrics[1], ref_df.shape[0], sub_df.shape[0])
-    
- # fix NANs
-full_df.fillna(0, inplace=True)
+
+
 
 # run it
-ann = ANN(full_df.shape[1])
-ann.set_data(full_df)
-rescaled_accuracy = ann.check_for_anomaly()
-
-#update state and value
-es.update(index=alarm_index, doc_type=alarm_type, id=job_id, body={"doc": {"processed": "yes", "rescaled":rescaled_accuracy}})
+while (True):
+    (job_id, data) = getWorkload()
+    if job_id == 0:
+        print('All done.')
+        break
+    
+    ann = ANN(data)
+    rescaled_accuracy = ann.check_for_anomaly()
+    
+    #update state and value
+    body={"doc": {"processed": "yes"}}
+    if rescaled_accuracy != -999: body['doc']['rescaled'] = rescaled_accuracy
+    es.update(index=alarm_index, doc_type=alarm_type, id=job_id, body=body)
