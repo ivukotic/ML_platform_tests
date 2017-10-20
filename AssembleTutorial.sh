65aa709d1f7a7fdc6c9bc84b1478818a7963ebd0
Ilija Vukotic <ivukotic@uchicago.edu>
2017-10-19 20:28:11 -0500

    Update AssembleTutorial.sh
    added Ben's tutorial

--- AssembleTutorial.sh
+++ AssembleTutorial.sh
@@ -9,6 +9,19 @@ yes n | gunzip SUSY.csv.gz
 mkdir -p /data/tutorial/ilija
 cd /data/tutorial/ilija
 
+# Ben's tutorial
+mkdir -p /data/tutorial/Ben
+cd /data/tutorial/Ben
+wget -nc https://data.mendeley.com/archiver/pvn3xc3wy5?version=1 -O CaloGAN.zip
+yes n | unzip CaloGAN.zip
+
+mkdir -p /ML_platform_tests/tutorial/Ben
+cd /ML_platform_tests/tutorial/Ben
+git clone https://github.com/hep-lbdl/CaloGAN.git
+cd /ML_platform_tests/tutorial/Ben/CaloGAN/models
+echo "positron: '/data/tutorial/Ben/eplus.hdf5'" > particles.yaml
+# python -m train particles.yaml
+
 mkdir /root/.jupyter/
 wget https://raw.githubusercontent.com/ivukotic/ML_platform_tests/master/jupyter_notebook_config.py
 mv jupyter_notebook_config.py /root/.jupyter/jupyter_notebook_config.py
