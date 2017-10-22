af2c7427b2535b0f4439bf70722a7f5dafc59c7c
Ilija Vukotic <ivukotic@uchicago.edu>
2017-10-22 09:21:24 -0500

    Create jlab.sh
    only root and jlab

---
+++ jlab.sh
@@ -0,0 +1,11 @@
+# setup ROOT
+cd /opt/root/
+source bin/thisroot.sh
+# copy ROOT kernel into Jupyter
+mkdir -p /root/.local/share/jupyter/kernels
+cp -r $ROOTSYS/etc/notebook/kernels/root ~/.local/share/jupyter/kernels
+
+mkdir /root/.jupyter/
+wget https://raw.githubusercontent.com/ivukotic/ML_platform_tests/master/jupyter_notebook_config.py
+mv jupyter_notebook_config.py /root/.jupyter/jupyter_notebook_config.py
+jupyter lab --allow-root
