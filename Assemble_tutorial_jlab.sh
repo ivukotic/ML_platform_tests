979f50481390d00bfc8515a61ca400aa3ade501b
Ilija Vukotic <ivukotic@uchicago.edu>
2017-10-25 09:50:17 -0500

    Update Assemble_tutorial_jlab.sh
    adding root-pandas here.

--- Assemble_tutorial_jlab.sh
+++ Assemble_tutorial_jlab.sh
@@ -28,6 +28,10 @@ source bin/thisroot.sh
 # copy ROOT kernel into Jupyter
 mkdir -p /root/.local/share/jupyter/kernels
 cp -r $ROOTSYS/etc/notebook/kernels/root ~/.local/share/jupyter/kernels
+ 
+# install root-pandas and root-numpy
+pip2 install root-pandas
+pip3 install root-pandas
 
 mkdir /root/.jupyter/
 wget https://raw.githubusercontent.com/ivukotic/ML_platform_tests/master/jupyter_notebook_config.py
