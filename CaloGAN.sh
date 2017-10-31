f3524d5acdc4035682580750f30314b0fe2a12ab
Ilija Vukotic <ivukotic@uchicago.edu>
2017-10-30 22:10:52 -0500

    Update CaloGAN.sh
    fix

--- CaloGAN.sh
+++ CaloGAN.sh
@@ -11,4 +11,4 @@ cd /ML_platform_tests/tutorial/Ben/CaloGAN/models
 echo "positron: '/data/CaloGAN/inputs/eplus.hdf5'" > particles.yaml
 export SHELL=/bin/bash
 
-python -m train "$@" particles.yaml
+python train.py "$@" particles.yaml
