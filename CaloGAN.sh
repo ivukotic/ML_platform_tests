280bf8f0fe122c14fb725c0056eb88a5c99a53cb
Ilija Vukotic <ivukotic@uchicago.edu>
2017-10-30 21:54:51 -0500

    Create CaloGAN.sh
    to be used for large scale hyper-parameter tuning

---
+++ CaloGAN.sh
@@ -0,0 +1,14 @@
+# create input data directory
+mkdir -p /data/CaloGAN/inputs/
+cd /data/CaloGAN/inputs
+wget -nc https://data.mendeley.com/archiver/pvn3xc3wy5?version=1 -O CaloGAN.zip
+yes n | unzip CaloGAN.zip
+
+mkdir -p /ML_platform_tests/tutorial/Ben
+cd /ML_platform_tests/tutorial/Ben
+git clone https://github.com/hep-lbdl/CaloGAN.git
+cd /ML_platform_tests/tutorial/Ben/CaloGAN/models
+echo "positron: '/data/CaloGAN/inputs/eplus.hdf5'" > particles.yaml
+export SHELL=/bin/bash
+
+python -m train "$@" particles.yaml
