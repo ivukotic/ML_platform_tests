50fcc48f0fde8b255fd40d11a7f622b6b0b9f881
Ilija Vukotic <ivukotic@uchicago.edu>
2017-11-03 14:48:54 -0500

    Update CaloGAN.sh
    now starting from my own simplified copy

--- CaloGAN.sh
+++ CaloGAN.sh
@@ -4,11 +4,18 @@ cd /data/CaloGAN/inputs
 wget -nc https://data.mendeley.com/archiver/pvn3xc3wy5?version=1 -O CaloGAN.zip
 yes n | unzip CaloGAN.zip
 
-mkdir -p /ML_platform_tests/tutorial/Ben
-cd /ML_platform_tests/tutorial/Ben
-git clone https://github.com/hep-lbdl/CaloGAN.git
-cd /ML_platform_tests/tutorial/Ben/CaloGAN/models
-echo "positron: '/data/CaloGAN/inputs/eplus.hdf5'" > particles.yaml
+# create weight data directory
+mkdir -p /data/CaloGAN/weights
+
+#create output data directory
+mkdir -p /data/CaloGAN/outputs
+
+mkdir -p /ML_platform_tests/tutorial/CaloGAN
+cd /ML_platform_tests/tutorial/
+git clone https://github.com/ivukotic/sc2017_prp.git
+cd /ML_platform_tests/tutorial/sc2017_prp
 export SHELL=/bin/bash
 
-python train.py "$@"
+#python train.py "$@"
+#python generator.py /data/CaloGAN/weights/xxx /data/CaloGAN/outputs/images1.h5
+python "$@"
