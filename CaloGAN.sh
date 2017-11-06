5126b44127750df27866fca09654a2af5787bbaa
Ilija Vukotic <ivukotic@uchicago.edu>
2017-11-06 16:13:58 -0600

    Update CaloGAN.sh
    trying rook

--- CaloGAN.sh
+++ CaloGAN.sh
@@ -1,14 +1,14 @@
 # create input data directory
-mkdir -p /data/CaloGAN/inputs/
-cd /data/CaloGAN/inputs
+mkdir -p /data-rook/CaloGAN/inputs/
+cd /data-rook/CaloGAN/inputs
 wget -nc https://data.mendeley.com/archiver/pvn3xc3wy5?version=1 -O CaloGAN.zip
 yes n | unzip CaloGAN.zip
 
 # create weight data directory
-mkdir -p /data/CaloGAN/weights
+mkdir -p /data-rook/CaloGAN/weights
 
 #create output data directory
-mkdir -p /data/CaloGAN/outputs
+mkdir -p /data-rook/CaloGAN/outputs
 
 cd /ML_platform_tests/tutorial/
 git clone https://github.com/ivukotic/sc2017_prp.git
@@ -16,5 +16,5 @@ cd /ML_platform_tests/tutorial/sc2017_prp
 export SHELL=/bin/bash
 
 #python train.py "$@"
-#python generator.py /data/CaloGAN/weights/params_generator_epoch_049.hdf5 /data/CaloGAN/outputs/images1.h5
+#python generator.py /data-rook/CaloGAN/weights/params_generator_epoch_049.hdf5 /data-rook/CaloGAN/outputs/images1.h5
 python "$@"
