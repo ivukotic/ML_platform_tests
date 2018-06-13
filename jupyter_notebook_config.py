865c90c4966aaea938ad65d62cc6732ff63cdadb
Ilija Vukotic <ivukotic@cern.ch>
2018-06-13 11:34:56 -0500

    with pass

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -2,7 +2,7 @@
 c.NotebookApp.ip = '*'
 c.NotebookApp.notebook_dir = '/ML_platform_tests/tutorial/'
 c.NotebookApp.open_browser = False
-#c.NotebookApp.password = 'sha1:6560b65c5ced:d4fdd6629d4ac73085f52b7928a288f98e2af88c'
+c.NotebookApp.password = 'sha1:6560b65c5ced:d4fdd6629d4ac73085f52b7928a288f98e2af88c'
 # disables authentication
-c.NotebookApp.token = ''
+# c.NotebookApp.token = ''
 c.NotebookApp.port = 9999
