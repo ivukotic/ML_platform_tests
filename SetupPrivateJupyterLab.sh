1cc994fb7375791f4dc38ce735e82e358cd4d8f6
Ilija Vukotic <ivukotic@cern.ch>
2018-06-18 23:33:44 -0500

    removing module adding private lab setup.

---
+++ SetupPrivateJupyterLab.sh
@@ -0,0 +1,19 @@
+# setup ROOT
+cd /opt/root/
+source bin/thisroot.sh
+# copy ROOT kernel into Jupyter
+mkdir -p /root/.local/share/jupyter/kernels
+cp -r $ROOTSYS/etc/notebook/kernels/root ~/.local/share/jupyter/kernels
+ 
+# install root-pandas and root-numpy
+pip2 install root-pandas
+pip3 install root-pandas
+
+mkdir /root/.jupyter/
+wget https://raw.githubusercontent.com/ivukotic/ML_platform_tests/master/jupyter_notebook_config.py
+mv private_jupyter_notebook_config.py /root/.jupyter/jupyter_notebook_config.py
+python add_lozinka.py "$@"
+
+export SHELL=/bin/bash
+
+jupyter lab --allow-root
---
+++ add_lozinka.py
@@ -0,0 +1,7 @@
+import sys
+from notebook.auth import passwd
+
+lozinka = passwd(sys.argv[1])
+
+with open("/root/.jupyter/jupyter_notebook_config.py", "a") as myfile:
+    myfile.write("c.NotebookApp.password = '" + lozinka + "'")
