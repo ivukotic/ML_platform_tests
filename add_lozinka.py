8aac9e7a52781186d480d3d03849466888b0b59f
Ilija Vukotic <ivukotic@cern.ch>
2018-06-19 10:29:58 -0500

    fixes

--- add_lozinka.py
+++ add_lozinka.py
@@ -4,4 +4,4 @@ from notebook.auth import passwd
 lozinka = passwd(sys.argv[1])
 print('adding hash', lozinka)
 with open("/root/.jupyter/jupyter_notebook_config.py", "a") as myfile:
-    myfile.write("c.NotebookApp.password = '" + lozinka + "'")
+    myfile.write("c.NotebookApp.password = '" + lozinka + "'\n")
