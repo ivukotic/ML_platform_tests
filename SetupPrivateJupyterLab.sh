ebd6f8b96ac1398255ccee6437feb7703f890dc3
Ilija Vukotic <ivukotic@cern.ch>
2018-06-19 12:57:33 -0500

    cleanup

--- SetupPrivateJupyterLab.sh
+++ SetupPrivateJupyterLab.sh
@@ -9,6 +9,7 @@ cp -r $ROOTSYS/etc/notebook/kernels/root ~/.local/share/jupyter/kernels
 pip2 install root-pandas
 pip3 install root-pandas
 
+mkdir /workspace/
 mkdir /root/.jupyter/
 wget https://raw.githubusercontent.com/ivukotic/ML_platform_tests/master/private_jupyter_notebook_config.py
 mv private_jupyter_notebook_config.py /root/.jupyter/jupyter_notebook_config.py
--- private_jupyter_notebook_config.py
+++ private_jupyter_notebook_config.py
@@ -1,4 +1,4 @@
 c.NotebookApp.ip = '*'
-c.NotebookApp.notebook_dir = '/ML_platform_tests/'
+c.NotebookApp.notebook_dir = '/workspace/'
 c.NotebookApp.open_browser = False
 c.NotebookApp.port = 9999
