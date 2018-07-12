55cb8bdbe1c4620133a94f59d354e75cdb0484e7
Ilija Vukotic <ivukotic@cern.ch>
2018-07-12 15:28:22 -0500

    dbg

--- add_lozinka.py
+++ add_lozinka.py
@@ -1,6 +1,6 @@
 import sys
 from notebook.auth import passwd
-
+print('lozinka:', sys.argv[1])
 lozinka = passwd(sys.argv[1])
 print('adding hash', lozinka)
 with open("/root/.jupyter/jupyter_notebook_config.py", "a") as myfile:
