062243ec8b2d9780abb25f499193d5a973b42206
Ilija Vukotic <ivukotic@cern.ch>
2019-01-18 12:00:27 -0600

    here too

--- public_jupyter_notebook_config.py
+++ public_jupyter_notebook_config.py
@@ -1,5 +1,5 @@
 # The IP address the notebook server will listen on.
-c.NotebookApp.ip = '*'
+c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.notebook_dir = '/workspace/AnalyticsNotebooks/'
 c.NotebookApp.open_browser = False
 c.NotebookApp.password = 'sha1:dd2cc8fc36f0:d2f7e95fd5c290871c74631bfaec8468a2a778c4'
