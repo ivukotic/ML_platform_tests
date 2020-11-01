43b7ff54b1bf58a018d1078fe2e7d5f73d0bde5f
Ilija Vukotic <ivukotic@cern.ch>
2020-11-01 12:56:29 -0600

    fix

--- SetupPrivateJupyterLab.sh
+++ SetupPrivateJupyterLab.sh
@@ -6,11 +6,11 @@ mkdir -p /root/.local/share/jupyter/kernels
 cp -r $ROOTSYS/etc/notebook/kernels/root ~/.local/share/jupyter/kernels
  
 # install root-pandas and root-numpy
-pip2 install root-pandas
-pip3 install root-pandas
+python2 -m pip install root-pandas
+python3 -m pip install root-pandas
 
-pip2 install RISE
-pip3 install RISE
+# With RISE, a Jupyter notebook extension, you can instantly turn your jupyter notebook into a live reveal.js-based presentation.
+python3 -m pip install RISE
 jupyter-nbextension install rise --py --sys-prefix
 jupyter-nbextension enable rise --py --sys-prefix
 
@@ -20,7 +20,7 @@ wget https://raw.githubusercontent.com/ivukotic/ML_platform_tests/master/private
 mv private_jupyter_notebook_config.py /root/.jupyter/jupyter_notebook_config.py
 
 wget https://raw.githubusercontent.com/ivukotic/ML_platform_tests/master/add_lozinka.py
-python add_lozinka.py "$1"
+python3 add_lozinka.py "$1"
 
 if [ "$2" != "" ]; then
     echo "Git Repo $2 requested..."
