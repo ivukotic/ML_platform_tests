cea7baf19b7a61575f6f037ea1871f1d675f4a70
Ilija Vukotic <ivukotic@cern.ch>
2022-04-04 11:30:41 -0500

    cleanup

--- public_jupyter_notebook_config.py
+++
@@ -1,8 +0,0 @@
-# The IP address the notebook server will listen on.
-c.NotebookApp.ip = '0.0.0.0'
-c.NotebookApp.notebook_dir = '/workspace/AnalyticsNotebooks/'
-c.NotebookApp.open_browser = False
-c.NotebookApp.password = 'sha1:dd2cc8fc36f0:d2f7e95fd5c290871c74631bfaec8468a2a778c4'
-# disables authentication
-# c.NotebookApp.token = ''
-c.NotebookApp.port = 9999
