59da0ca24bdd13af4a86e3a1918ffbefae770590
Ilija Vukotic <ivukotic@cern.ch>
2022-12-13 17:26:29 -0600

    fixing ROOT issue

--- SetupPrivateJupyterLab.sh
+++ SetupPrivateJupyterLab.sh
@@ -29,10 +29,11 @@ if [ "$OWNER" != "" ] && [ "$CONNECT_GROUP" != "" ]; then
     cp -r /ML_platform_tests/tutorial ~/.
 
     # setup ROOT
-    # cd /opt/root/
-    # source bin/thisroot.sh
-    # mkdir -p ~/.local/share/jupyter/kernels
-    # cp -r $ROOTSYS/etc/notebook/kernels/root ~/.local/share/jupyter/kernels
+    cd /opt/root/
+    source bin/thisroot.sh
+    unset JUPYTER_PATH
+    mkdir -p ~/.local/share/jupyter/kernels
+    cp -r $ROOTSYS/etc/notebook/kernels/root ~/.local/share/jupyter/kernels
 
     python3.8 -m pip --no-cache-dir install  root-pandas 
 
